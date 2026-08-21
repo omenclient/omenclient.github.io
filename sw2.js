cc2e8f273aea1b4b5303fff31352adb3459de18c
omenclient <273393699+omenclient@users.noreply.github.com>
2026-08-22 00:33:30 +0530

    Updates

--- sw2.js
+++ sw2.js
@@ -1,4 +1,4 @@
-const cacheName = "1786705292801"; // this gets replaced by the build script
+const cacheName = "1787339003885"; // this gets replaced by the build script
 
 self.addEventListener("install", (e) => {
   console.log("[Service Worker] Install", cacheName);
